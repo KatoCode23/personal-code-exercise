86dd157ca124e0157942a4c07139fea02cae8296
KatoCode23 <Kalen23Imani@gmail.com>
2019-10-14 13:36:33 -0400

    i tunes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+console.log('hello world');
+
+async function getData(){
+    document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+
+        //prevent the normal submission of the form
+        e.preventDefault();    
+    });
+
+    var artist = document.getElementById('searchArtist').value
+    var str = 'https://itunes.apple.com/search?entity=album&term=ARTIST_NAME';
+    var url = str.replace('ARTIST_NAME', artist);
+    var request = new Request(url);
+    var response = await fetch(request);
+    var responseBody = await response.json();
+    data = responseBody;
+    results = data.results;
+    document.getElementById('container').innerText = results;
+    console.log(results);
+  }
